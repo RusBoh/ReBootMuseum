c969619ac910d3f4892c8626c96694b4c991f66b
Bohdan Rusanov <bgdnrsnv@gmail.com>
2026-03-13 20:51:57 +0100

    add getting data from db

--- code/python/ReBootMuseumDev/reboot_museum/homeapp/views.py
+++ code/python/ReBootMuseumDev/reboot_museum/homeapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from .models import *
 
 def home(request):
-    return render(request, 'pages/home/index.html')
+    rooms = Room.objects.all().values()
+    exhibits = Exhibit.objects.all().values()
+    categories = Category.objects.all().values()
+
+    context = {
+        'rooms': rooms,
+        'exhibits': exhibits,
+        'categories': categories,
+    }
+
+    return render(request, 'pages/home/index.html', context=context)
